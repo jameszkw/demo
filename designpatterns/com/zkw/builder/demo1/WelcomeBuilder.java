476926a3fa32fd0b547a1cdf2c3a21d29e60ce22
james58 <905119621@qq.com>
2016-04-21 00:11:13 +0800

    design patterns builder

---
+++ designpatterns/com/zkw/builder/demo1/WelcomeBuilder.java
@@ -0,0 +1,20 @@
+package com.zkw.builder.demo1;
+
+/**
+ * Created by Administrator on 2016/4/20 0020.
+ */
+public class WelcomeBuilder extends Builder{
+    public WelcomeBuilder(){
+        msg = new WelcomeMessage();
+    }
+
+    @Override
+    public void buildSubject() {
+        msg.setSubject("welcome subject");
+    }
+
+    @Override
+    public void buildBody() {
+        msg.setBody("welcome body");
+    }
+}
---
+++ designpatterns/com/zkw/builder/demo2/InsuranceContract.java
@@ -0,0 +1,106 @@
+package com.zkw.builder.demo2;
+
+/**
+ * 使用建造模式构建复杂对象
+ * Created by Administrator on 2016/4/20 0020.
+ * 保险合同对象
+ */
+public class InsuranceContract {
+    //保险合同编号
+    private String contractId;
+    /*
+    * 被保险人员的名称，同一份保险合同，要么跟人员签订，要么跟公司签订
+    * 也就是说，“被保险人员”和“被保险公司”这俩属性不能同时有值
+    * */
+    private String personName;
+    //被保险公司的名称
+    private String companyName;
+    //保险开始生效日期
+    private long beginDate;
+    //保险失效日期，一定会大于保险开始生效日期
+    private long endDate;
+    //其他数据
+    private String otherData;
+
+    private InsuranceContract(ConcreteBuilder builder) {
+        this.contractId = builder.contractId;
+        this.personName = builder.personName;
+        this.companyName = builder.companyName;
+        this.beginDate = builder.beginDate;
+        this.endDate = builder.endDate;
+        this.otherData = builder.otherData;
+
+    }
+
+    public void someOperation(){
+        System.out.println("当前正在操作的保险合同编号为【"+this.contractId+"】");
+    }
+
+    public static class ConcreteBuilder{
+        private String contractId;
+        private String personName;
+        private String companyName;
+        private long beginDate;
+        private long endDate;
+        private String otherData;
+
+        /**
+         * 构造方法，传入必须要有的参数
+         * @param contractId     保险合同编号
+         * @param beginDate        保险合同开始生效日期
+         * @param endDate        保险合同失效日期
+         */
+        public ConcreteBuilder(String contractId,long beginDate,long endDate){
+            this.contractId = contractId;
+            this.beginDate = beginDate;
+            this.endDate = endDate;
+        }
+        //被保险人员的名称
+        public ConcreteBuilder setPersonName(String personName){
+            this.personName = personName;
+            return this;
+        }
+        //被保险公司的名称
+        public ConcreteBuilder setCompanyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        //其他数据
+        public ConcreteBuilder setOtherData(String otherData) {
+            this.otherData = otherData;
+            return this;
+        }
+
+        /**
+         * 构建真正的对象并返回
+         * @return    构建的保险合同对象
+         */
+        public InsuranceContract build(){
+            if (contractId == null || contractId.trim().length() == 0){
+                throw new IllegalArgumentException("合同编号不能为空");
+            }
+            boolean signPerson = personName != null && personName.trim().length()>0;
+            boolean signCompany = companyName != null && companyName.trim().length()>0;
+            if (signPerson && signCompany){
+                throw  new IllegalArgumentException("一份保险合同不能同时与个人和公司签订");
+            }
+            if (signPerson == false && signCompany == false){
+                throw new IllegalArgumentException("一份保险合同不能没有签订对象");
+            }
+            if (beginDate <= 0){
+                throw new IllegalArgumentException("一份保险合同必须有开始生效的日期");
+            }
+            if (endDate <= 0){
+                throw new IllegalArgumentException("一份保险合同必须有失效的日期");
+            }
+            if (beginDate > endDate){
+                throw new IllegalArgumentException("一份保险合同的失效日期必须大于生效日期");
+            }
+            return new InsuranceContract(this);
+        }
+
+    }
+
+
+}
